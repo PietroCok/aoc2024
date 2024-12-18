210d5783799689f92006569bf232eb877399b11c
PietroCok <pietrocok97@gmail.com>
2024-12-18 20:15:50 +0100

    day 16 added more functionalities to visualization
    added buttons to start and stop the animation
    added selection for turn cost, 1000 (default as per puzzles request) - 0 (classic shortest path)

--- day16/index.js
+++ day16/index.js
@@ -4,14 +4,14 @@ async function main() {
     // const data = await getData('test.txt');
     // const data = await getData('test_1.txt');
     const data = await getData('input.txt');
-
-    console.log(data);
-
+    //console.log(data);
 
     let maze = data.split('\r\n').map(r => r.split(''));
-    function reset(){
+    function reset() {
         maze = data.split('\r\n').map(r => r.split(''));
     }
+
+    let turn_cost = 1000;
 
     class Node {
         constructor(x, y, prevNode) {
@@ -42,16 +42,16 @@ async function main() {
             }
         }
         setCost() {
-            if(!this.prevNode){
+            if (!this.prevNode) {
                 this.cost = 0;
             } else {
                 this.cost = this.prevNode.cost + 1;
-                if(shortestPathNodes.size > 0 && shortestPathNodes.has(`${this.x}|${this.y}`)){
+                if (shortestPathNodes.size > 0 && shortestPathNodes.has(`${this.x}|${this.y}`)) {
                     this.cost += 0.001;
                 }
-                if(this.direction != this.prevNode.direction){
+                if (this.direction != this.prevNode.direction) {
                     // update previous node cose
-                    this.cost += 1000;
+                    this.cost += turn_cost;
                 }
             }
         }
@@ -80,47 +80,58 @@ async function main() {
         }
     }
 
+    let running = true;
     const canvas = new Canvas(maze);
+    let shortestPathNodes;
 
-
-    const shortestPathNodes = new Set();
-    canvas.draw();
-    //console.log(startNode, target);
-    const path = await solveDijkstra();
-    drawPath(path);
-    console.log(`Lowest maze score is: ${path[path.length - 1]?.cost || 'error'}`);
-
-    for (const node of path) {
-        shortestPathNodes.add(`${node.x}|${node.y}`);
-    }
-
-    let count = 1;
-    while (true) {
-        let same = true;
+    async function start() {
         reset();
-        const _path = await solveDijkstra();
+        shortestPathNodes = new Set();
+        canvas.draw();
+        //console.log(startNode, target);
+        const path = await solveDijkstra();
+        drawPath(path);
+        if(!running) return;
+        console.log(`Lowest maze score is: ${path[path.length - 1]?.cost || 'error'}`);
 
-        for (const node of _path) {
-            if(!shortestPathNodes.has(`${node.x}|${node.y}`)){
-                same = false;
-                shortestPathNodes.add(`${node.x}|${node.y}`);
+        for (const node of path) {
+            shortestPathNodes.add(`${node.x}|${node.y}`);
+        }
+
+        let count = 1;
+        while (true) {
+            let same = true;
+            reset();
+            const _path = await solveDijkstra();
+
+            for (const node of _path) {
+                if (!shortestPathNodes.has(`${node.x}|${node.y}`)) {
+                    same = false;
+                    shortestPathNodes.add(`${node.x}|${node.y}`);
+                }
             }
+
+            drawPath(_path);
+
+            if (same) {
+                console.log(`Shortest paths found: ${count}, total tiles: ${shortestPathNodes.size}`);
+                break;
+            }
+
+            count++;
         }
-
-        drawPath(_path);
-
-        if(same){
-            console.log(`Shortest paths found: ${count}, total tiles: ${shortestPathNodes.size}`);
-            break;
-        }
-
-        count++;
+        // draw all path found
+        canvas.draw(Array.from(shortestPathNodes).map(n => {
+            const node = new Node(n.split('|')[0], n.split('|')[1]);
+            return node;
+        }));
+        _stop();
     }
-    
+
     async function solveDijkstra() {
         let totalNodes = 2;
         for (const row of maze) {
-            for (const  col of row) {
+            for (const col of row) {
                 if (col == '.') {
                     totalNodes++;
                 }
@@ -130,41 +141,45 @@ async function main() {
         const visited = new Map(); // To track visited nodes
         const queue = [];
         queue.push(startNode);
-        
+
         while (queue.length > 0) {
-            
+
+            if (!running) {
+                return [];
+            }
+
             // sort node based on cost
             queue.sort((a, b) => a.cost - b.cost);
             let currentNode = queue.shift(); // Get the node with the lowest cost
 
-            
+
             await new Promise(resolve => setTimeout(resolve, 0));
             canvas.draw(getPath(currentNode), visited);
-            
+
             // Check if current node is the target
             if (currentNode.x == target.x && currentNode.y == target.y) {
                 console.log('Maze exit found!');
                 return getPath(currentNode);
             }
-            
+
             // Skip if node has already been visited
             if (visited.has(`${currentNode.x}|${currentNode.y}|${currentNode.direction}`)) {
                 continue;
             }
-            
+
             //console.log(`Nodes checked: ${visited.size}/${totalNodes}`, queue.length);
             visited.set(`${currentNode.x}|${currentNode.y}|${currentNode.direction}`, currentNode);
-    
+
             // Find adjacent nodes
             const adiacentNodes = [];
             for (const coords of availableDirections) {
                 let _x = currentNode.x + coords[0];
                 let _y = currentNode.y + coords[1];
-    
+
                 // Exclude walls
                 if (maze[_y][_x] == '#') continue;
                 if (currentNode.prevNode && _x == currentNode.prevNode.x && _y == currentNode.prevNode.y) continue;
-                    
+
                 let _node = new Node(_x, _y, currentNode);
                 adiacentNodes.push(_node);
 
@@ -182,12 +197,12 @@ async function main() {
                 }
             }
         }
-    
+
         console.log('Path not found!');
         return [];
     }
 
-    function getPath(node){
+    function getPath(node) {
         const path = [];
         let current = node;
         while (current) {
@@ -198,11 +213,41 @@ async function main() {
     }
 
     function drawPath(path) {
+        if(!path) return;
         let _maze = JSON.parse(JSON.stringify(maze))
         for (const node of path) {
             _maze[node.y][node.x] = node.direction;
         }
-        console.log(_maze.map(row => row.join('')).join('\r\n'));
+        //console.log(_maze.map(row => row.join('')).join('\r\n'));
+    }
+
+
+    const start_button = document.querySelector('#start');
+    const stop_button = document.querySelector('#stop');
+    const cost_selection = document.querySelector('#cost');
+
+    start_button.onclick = _start;
+    stop_button.onclick = _stop;
+
+    function _start(){
+        start_button.disabled = true;
+        cost_selection.disabled = true;
+        stop_button.removeAttribute('disabled');
+        running = true;
+        start();
+    }
+
+    function _stop(){
+        running = false;
+        start_button.removeAttribute('disabled');
+        cost_selection.removeAttribute('disabled');
+
+        stop_button.disabled = true;
+    }
+
+
+    cost_selection.onchange = () => {
+        turn_cost = cost_selection.value;
     }
 }
 
